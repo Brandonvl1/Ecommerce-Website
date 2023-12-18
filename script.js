d228409b582b8a807da28e346363bf7de62eccda
Brandonvl1 <brandonvloggerenberg1@gmail.com>
2023-12-18 11:12:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let MenuItems = document.getElementById('MenuItems');
+
+MenuItems.style.maxHeight = '0px';
+
+function menuToggle(){
+    if(MenuItems.style.maxHeight == '0px'){
+        MenuItems.style.maxHeight = '200px';
+    }
+    else{
+        MenuItems.style.maxHeight = '0px';
+    }
+}
+
+
+// ----------------JS for Product Gallery ----------------
+
+let productImg = document.getElementById('productImg');
+let smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function(){
+    productImg.src = smallImg[0].src
+}
+smallImg[1].onclick = function(){
+    productImg.src = smallImg[1].src
+}
+smallImg[2].onclick = function(){
+    productImg.src = smallImg[2].src
+}
+smallImg[3].onclick = function(){
+    productImg.src = smallImg[3].src
+}
